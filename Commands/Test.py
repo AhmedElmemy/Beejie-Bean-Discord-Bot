c7d965dcb17498415760941bc4501b2fdd7bd04a
AhmedElmemy <52626211+AhmedElmemy@users.noreply.github.com>
2020-07-25 17:50:26 +0200

    Update Test.py

--- Commands/Test.py
+++ Commands/Test.py
@@ -1,1 +1,17 @@
+import discord, random
+import discord.ext.commands as client
+from discord.ext.commands.cooldowns import BucketType
 
+
+class Test(client.Cog):
+
+    def __init__(self, bot):
+         self.bot = bot
+
+    @client.command()
+    @client.cooldown(1, 2, BucketType.user)
+    async def test(self, ctx):
+        await ctx.send(f"Working perfectly fine. <:Beejie:612618277650432001>")
+
+def setup(bot):
+    bot.add_cog(Test(bot))
