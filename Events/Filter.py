e59abe21913ad472c8f0347b7fc2de16965c79e3
AhmedElmemy <52626211+AhmedElmemy@users.noreply.github.com>
2020-07-25 17:54:00 +0200

    Update Filter.py

--- Events/Filter.py
+++ Events/Filter.py
@@ -1,1 +1,25 @@
+import discord, re
+import discord.ext.commands as client
+from discord.ext.commands.cooldowns import BucketType
 
+
+class Message(client.Cog):
+    def __init__(self, bot):
+
+        self.bot = bot
+
+    @client.Cog.listener()
+    async def on_message(self, message):
+        
+        if re.search(r"(Nigga|Nigger|Faggot|Shemale|Tranny|Vomit)", message.content, re.IGNORECASE):
+            await message.delete()
+            censor = discord.Embed(
+                title = '**You have been warned**',
+                description = 'Avoid using this type of language or you\'ll be banned',
+                colour = 0xff0000 )
+
+            censor.set_footer(text=f"Please don't use this kind of offensive language!")
+            await message.channel.send(embed=censor) 
+
+def setup(bot):
+    bot.add_cog(Message(bot))
