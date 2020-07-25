c80970ca9850e3f33844d3ee89d734af7b1cbeea
AhmedElmemy <52626211+AhmedElmemy@users.noreply.github.com>
2020-07-25 17:47:11 +0200

    An actual webserver.... right?

--- webserver.py
+++ webserver.py
@@ -1,1 +1,16 @@
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+#e
+@app.route('/')
+def home():
+  return"Webserver OK, Discord bot ok"
+  
+def run():
+  app.run(host="0.0.0.0",port=8080)
+      
+def keep_alive():
+  t=Thread(target=run)
+  t.start()
